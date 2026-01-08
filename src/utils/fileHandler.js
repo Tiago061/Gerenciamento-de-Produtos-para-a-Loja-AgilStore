3c180efaf3efc0cb742707932df8be8f698735c3
Tiago Henrique <thenriqueleite277@gmail.com>
2026-01-08 12:26:40 -0300

    feat(utils): add fileHandler.js

--- src/utils/fileHandler.js
+++ src/utils/fileHandler.js
@@ -1,1 +1,32 @@
 import path from 'path';
+import fs from 'fs/promises';
+
+const filePath = path.join(__dirname, '../data/produtos.json');
+
+export default function saveData(data){
+    try{
+        fs.writeFileSync(filePath, JSON.stringify(data, null, 2))
+    }catch(error){
+        console.error('Erro ao salvar os dados:', error.message)
+    }
+    
+}
+
+export default function loadData() {
+    try{
+        if(!fs.existsSync(filePath)){
+            fs.writeFileSync(filePath, JSON.stringify([], null, 2));
+            return [];
+        }
+        const data = fs.readFileSync(filePath, 'utf-8')
+        return data ? JSON.parse(data) : []
+    } catch(error){
+        console.error('Erro ao carregar os dados:', error.message)
+        return []
+    }
+}
+
+
+
+
+
